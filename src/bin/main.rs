4e5597186c9e96a9359f96be1902d520f38b2c18
Otto J. Hermann <30403309+ojhermann@users.noreply.github.com>
2021-05-17 15:33:33 +0100

    Modify gurl crud methods to reference pooled_connections (#5)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,3 @@
-use guardian_backend::data::database_pool;
-
 fn main() {
     dotenv::dotenv().ok();
-
-    let dp = database_pool::get("DATABASE_URL");
-    let pooled_connection = dp.get().unwrap();
 }
