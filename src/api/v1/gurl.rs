8cb3884376c28273c92802e357cef5e964741c75
Otto Hermann <ojhermann@gmail.com>
2021-05-19 21:58:14 +0100

    Change gurl api to use json instead of path variables

--- src/api/v1/gurl.rs
+++ src/api/v1/gurl.rs
@@ -2,11 +2,45 @@ use crate::data::database_pool::DatabasePool;
 use crate::data::gurl::Gurl;
 use actix_web::{web, HttpResponse};
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DeleteGurl {
+    pub id: i32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GetGurl {
+    pub url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct InsertGurl {
+    pub url: String,
+    pub liked: bool,
+}
+
+pub async fn get_gurl(
+    database_pool: web::Data<DatabasePool>,
+    json: web::Json<GetGurl>,
+) -> HttpResponse {
+    let url = json.into_inner().url;
+
+    let pooled_connection = database_pool
+        .get()
+        .expect("get_gurl_json: could not obtain a connection from the pool");
+
+    let gurls_result = web::block(move || Gurl::get(url, &pooled_connection)).await;
+
+    match gurls_result {
+        Ok(vector_of_gurls) => HttpResponse::Ok().json(vector_of_gurls),
+        Err(_e) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
 pub async fn delete_gurl(
     database_pool: web::Data<DatabasePool>,
-    id_value: web::Path<i32>,
+    json: web::Json<DeleteGurl>,
 ) -> HttpResponse {
-    let id_value = id_value.into_inner();
+    let id_value = json.into_inner().id;
 
     let pooled_connection = database_pool
         .get()
@@ -20,31 +54,13 @@ pub async fn delete_gurl(
     }
 }
 
-pub async fn get_gurl(
-    database_pool: web::Data<DatabasePool>,
-    url_value: web::Path<String>,
-) -> HttpResponse {
-    let url_value = url_value.into_inner();
-
-    let pooled_connection = database_pool
-        .get()
-        .expect("get_gurl: could not obtain a connection from the pool");
-
-    let gurls_result = web::block(move || Gurl::get(url_value, &pooled_connection)).await;
-
-    match gurls_result {
-        Ok(vector_of_gurls) => HttpResponse::Ok().json(vector_of_gurls),
-        Err(_e) => HttpResponse::InternalServerError().finish(),
-    }
-}
-
 pub async fn insert_gurl(
     database_pool: actix_web::web::Data<DatabasePool>,
-    path_params: web::Path<(String, bool)>,
+    json: web::Json<InsertGurl>,
 ) -> HttpResponse {
-    let path_params = path_params.into_inner();
-    let url_value = path_params.0;
-    let liked = path_params.1;
+    let insert_gurl = json.into_inner();
+    let url_value = insert_gurl.url;
+    let liked = insert_gurl.liked;
 
     let pooled_connection = database_pool
         .get()
